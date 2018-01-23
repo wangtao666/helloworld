88388171db6f81a780a0537becfcb01e553c27db
jiaxiaoyan <jiaxiaoyan>
2018-01-23 15:36:46 +0800

    路由

--- mynuxt/components/myGroups/groups.vue
+++ mynuxt/components/myGroups/groups.vue
@@ -2,8 +2,8 @@
 	<div>
 		 <header class="header">
 		 	<!--[myGroups,currGroups]-->
-	        <nuxt-link to="/myGroups/fightGroups"  :class="isA ? 'myGroups currGroups':'myGroups'">我的拼团</nuxt-link>
-	        <nuxt-link  to="/myGroups/myOffered"  :class="isB ? 'myGroups currGroups':'myGroups'">我的参团</nuxt-link>
+	        <nuxt-link to="/myGroups"  :class="isA ? 'myGroups currGroups':'myGroups'">我的拼团</nuxt-link>
+	        <nuxt-link  to="/myOffered"  :class="isB ? 'myGroups currGroups':'myGroups'">我的参团</nuxt-link>
         </header>
 	</div>
 </template>
