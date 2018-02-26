ef32a9fea7188bc040c745c069964642dbea706f
jiaxiaoyan <jiaxiaoyan>
2018-02-26 15:20:02 +0800

    拼团的加载更多,分页

--- mynuxt/pages/myGroups/index.vue
+++ mynuxt/pages/myGroups/index.vue
@@ -7,16 +7,26 @@
       </div>
       </section>
 		<div class="content">
-			<!--子组件，显示不同的 tab   is 特性动态绑定子组件    keep-alive 将切换出去的组件保留在内存中-->
-			<!--拼团进行中-->
-			<underWay :is="currentTab" :fightData='fightData' :hide='hide' :hideBtn='hideBtn' keep-alive></underWay>
-			<!--拼团成功-->
-			<!--<fightSuccess/>-->
+          <div slot="top" class="mint-loadmore-top">
+            <span v-show="topStatus !== 'loading'" :class="{ 'rotate': topStatus === 'drop' }">刷新</span>
+            <span v-show="topStatus === 'loading'">加载中...</span>
+          </div>
+          <!--子组件，显示不同的 tab   is 特性动态绑定子组件    keep-alive 将切换出去的组件保留在内存中-->
+          <!--拼团进行中-->
+          <underWay :is="currentTab" :fightData='fightData' :hide='hide' :hideBtn='hideBtn' keep-alive></underWay>
+          <!--拼团成功-->
+          <!--<fightSuccess/>-->
+          <div slot="bottom" class="mint-loadmore-bottom">
+            <span v-show="bottomStatus !== 'loading'" :class="{ 'rotate': bottomStatus === 'drop' }">释放后加载</span>
+            <span v-show="bottomStatus === 'loading'">加载中...</span>
+          </div>
+
 		</div>
 	</div>
 </template>
 
 <script>
+  import { Loadmore } from 'mint-ui';
 	import groups from '../../components/myGroups/groups'
 	import underWay from '../../components/myGroups/underWay'
 	import fightSuccess from '../../components/myGroups/fightSuccess'
@@ -44,7 +54,13 @@
           {"name":"拼团失败","group":"collageFailure"},
           {"name":"拼团完成","group":"collageFull"}],
         "indexGroup":"0",
-        fightData:''
+        fightData:'',
+        paging:{  //分页属性
+          currentPage:"0",  //当前页
+        },
+        allLoaded: false, //是否可以上拉属性，false可以上拉，true为禁止上拉，就是不让往上划加载数据了
+        topStatus:"",
+        bottomStatus:''
 			}
 		},
 		async asyncData() {
@@ -68,7 +84,49 @@
               this.fightData=data
             })
           }
-			}
+			},
+      //			分页查询(加载更多)
+      handleTopChange(status) {
+        this.topStatus = status;
+
+        console.log("status", this.topStatus)
+      },
+      handleBottomChange(status){
+        this.bottomStatus=status
+        console.log("status1",this.bottomStatus)
+      },
+      loadTop:function() { //组件提供的下拉触发方法
+        //下拉加载
+        this.loadPageList(0);
+        this.$refs.loadmore.onTopLoaded();// 固定方法，查询完要调用一次，用于重新定位
+      },
+      loadBottom:function() {
+        // 上拉加载
+        // 上拉触发的分页查询
+        this.paging.currentPage=parseInt(this.paging.currentPage)+1;
+        this.loadPageList(this.paging.currentPage);
+        this.$refs.loadmore.onBottomLoaded();// 固定方法，查询完要调用一次，用于重新定位
+      },
+      loadPageList:function(currentPage){
+        this.paging.currentPage=currentPage;
+        api.get  ('/msg1.json',{"currentPage":this.paging.currentPage}).then(({ data }) => {
+
+          if(this.paging.currentPage==0){
+            this.fightData=data;
+          }else{
+            var data=data.date
+            if(data.length<=0 || JSON.stringify(data[0])=='{}'){
+
+              this.allLoaded = true; //数据已全部获取完毕
+            }else{
+              for(var value of data){
+                this.fightData.date.push(value);
+              }
+
+            }
+          }
+        })
+      }
 		}
 
 	}
@@ -76,5 +134,5 @@
 
 <style scoped>
 	@import "../../assets/css/base.css";
-	@import "../../static/css/myGroups.css";
+	@import "../../assets/css/myGroups.css";
 </style>
